9363b1ec717880ff7eb756a18531c9ca358066fd
srushti <srushti20@navgurukul.org>
2023-11-24 19:31:27 +0530

    login page add

--- app/navbar.js
+++ app/navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+
 const Navbar=()=>{
     return(
         <div className='NavBar'>
         <Link href="/about" passHref legacyBehavior>
           <a>About</a>
+        </Link>
+        <Link href="/login" passHref legacyBehavior>
+          <a>Login</a>
         </Link>
   </div>
   )
